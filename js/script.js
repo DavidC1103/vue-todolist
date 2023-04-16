63720548f60519355b83ad255d4531c1a5e02544
David Calaiò <davidcjr11@gmail.com>
2023-04-16 16:47:39 +0200

    add object in js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,25 @@ const {createApp} = Vue;
 
 createApp({
 
-data(){
-    return{
-        
+    data(){
+        return{
+            tasks:[
+            {
+                text:'Ciao',
+                done:true
+            },
+            {
+                text:'Ciaooooo',
+                done:false
+            },
+            {
+                text:'Ciaoooo',
+                done:true
+            },
+        ]
 
+        }
     }
-}
 
 
 
